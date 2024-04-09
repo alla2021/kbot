242ee20d0e4ef59304bb6560234e035633f2eacc
Alla Honcharova <alla7gon@gmail.com>
2024-04-09 17:39:06 +0300

    added fix

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-# kbot
+# Go_hi_bot Project
+
+This project is created to develop a Telegram bot using the Golang programming language that can respond to user messages.
+
+## Bot Link
+
+You can use the following link to add the bot to your Telegram:
+
+[Go_hi_bot](https://t.me/Go_hi_bot)
+
+## Installation Instructions
+
+1. Clone the repository:
+
+    ```bash
+    git clone https://github.com/your_username/Go_hi_bot.git
+    ```
+
+2. Navigate to the project directory:
+
+    ```bash
+    cd Go_hi_bot
+    ```
+
+3. Install dependencies:
+
+    ```bash
+    go mod tidy
+    ```
+
+4. Set the bot token in the TELE_TOKEN environment variable:
+
+    ```bash
+    export TELE_TOKEN=your_token
+    ```
+
+5. Run the bot:
+
+    ```bash
+    go run main.go
+    ```
+
+## Examples of Command Usage
+
+After adding the bot to your Telegram, you can use the following commands:
+
+- `/hello`: The bot will send a greeting "Hello!".
+
+The bot is always ready to communicate with you!
+
---
+++ main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/spf13/cobra"
+	tele "gopkg.in/telebot.v3"
+)
+
+func main() {
+	// Initializing root command using Cobra
+	var rootCmd = &cobra.Command{
+		Use:   "kbot",
+		Short: "Telegram bot with root command",
+		Run: func(cmd *cobra.Command, args []string) {
+			// Starting the Telegram bot
+			startTelegramBot()
+		},
+	}
+
+	// Adding root command to Cobra
+	var cmdKbot = &cobra.Command{Use: "kbot"}
+	rootCmd.AddCommand(cmdKbot)
+
+	// Calling the root command
+	if err := rootCmd.Execute(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func startTelegramBot() {
+	// Getting bot token from environment variable
+	teleToken :=  os.Getenv("TELE_TOKEN")
+	if teleToken == "" {
+		log.Fatal("Bot token value not set for Telegram.")
+	}
+
+	// Telegram bot settings
+	pref := tele.Settings{
+		Token:  teleToken,
+		Poller: &tele.LongPoller{Timeout: 10 * time.Second},
+	}
+
+	// Creating a new Telegram bot object
+	b, err := tele.NewBot(pref)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Handler for the /start command
+	b.Handle("/start", func(c tele.Context) error {
+		return c.Send("Welcome! This is the start command.")
+	})
+
+	// Handler for the /help command
+	b.Handle("/help", func(c tele.Context) error {
+		return c.Send("This is the help command. Here you can get help.")
+	})
+
+	// Handler for the /info command
+	b.Handle("/info", func(c tele.Context) error {
+		return c.Send("This is the info command. Here you can get information.")
+	})
+
+	// Starting the Telegram bot
+	b.Start()
+}
